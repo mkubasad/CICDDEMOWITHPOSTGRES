4f11443b3f4430b4123cbf1152097bb1a3607c84
Manjunatha K <mkubasad100@gmail.com>
2024-12-25 02:31:30 +0530

    debugging populatesystemdata step in yaml file

--- deploy/systemdata.sql
+++ deploy/systemdata.sql
@@ -1,2 +1,2 @@
-trucate demo_sequence;
-insert into demo_sequence(seq_type, prefix, last_number) values ("DR", "DR", 1000);
+truncate demo_sequence;
+insert into demo_sequence(seq_type, prefix, last_number) values("DR", "DR", 1000);
